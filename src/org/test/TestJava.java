8b069252f3accfa90b7aed68b16b6ac995466b4d
dineshwebstar <dineshkrish16@gmail.com>
2021-03-07 21:18:29 +0530

    java done

---
+++ src/org/test/TestJava.java
@@ -0,0 +1,13 @@
+package org.test;
+
+public class TestJava {
+	public void empId() {
+		System.out.println("dinesh");
+
+	}
+	public static void main(String[] args) {
+		TestJava s=new TestJava();
+		s.empId();
+	}
+
+}
