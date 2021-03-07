5bfb82faa44e5af45b4431cc358b573037895198
dineshkrish16 <dineshkrish16@gmail.com>
2021-03-07 21:32:01 +0530

    sam done

--- src/org/test/TestJava.java
+++ src/org/test/TestJava.java
@@ -5,9 +5,14 @@ public class TestJava {
 		System.out.println("dinesh");
 
 	}
+	
+	public void comId() {
+		System.out.println("fff");
+	}
 	public static void main(String[] args) {
 		TestJava s=new TestJava();
 		s.empId();
+		s.comId();
 	}
 
 }
